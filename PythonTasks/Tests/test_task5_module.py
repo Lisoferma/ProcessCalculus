06e88897fb9c28287fdeef571eede90c870ef4d4
Lisoferma <pilipeiko.sasha@gmail.com>
2024-02-21 04:02:13 +0900

    Fix the check for equality of floating numbers

--- PythonTasks/Tests/test_task5_module.py
+++ PythonTasks/Tests/test_task5_module.py
@@ -1,5 +1,6 @@
 __author__ = "Lisoferma"
 
+import math
 import unittest
 from Modules import task5_module as module
 
@@ -10,7 +11,7 @@ class TestTask5Module(unittest.TestCase):
         data = [1, 2, 3, 4, 5]
         expected_sum = 4.375
 
-        self.assertEqual(module.calculate_sum(data), expected_sum)
+        self.assertTrue(math.isclose(module.calculate_sum(data), expected_sum))
 
     def test_calculate_sum__data_is_one_item__return_zero(self):
         data = [5]
