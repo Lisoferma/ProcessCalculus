2962a4e4c09288e5a8f6f568affae9922aa48a46
Lisoferma <pilipeiko.sasha@gmail.com>
2024-02-28 10:15:33 +0900

    Optimization

--- PythonTasks/Modules/task5_module.py
+++ PythonTasks/Modules/task5_module.py
@@ -2,8 +2,6 @@ __author__ = "Lisoferma"
 
 """Модуль содержит вспомогательные функции для задания 5."""
 
-import math
-
 
 def calculate_sum(data: list):
     """Вычислить сумму a[i] / i!, где i от 0 до n (n не включительно).
@@ -11,9 +9,10 @@ def calculate_sum(data: list):
     :param data: список данных для нахождения суммы.
     """
     result = 0
+    last_factorial = 1
 
-    # исп пред значение факториала
-    for i in range(0, len(data)):
-        result += data[i] / math.factorial(i)
+    for i in range(1, len(data)):
+        last_factorial *= i
+        result += data[i] / last_factorial
 
     return result
